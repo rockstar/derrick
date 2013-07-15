33f5a6b57e48d655021f2353dca76c93615a6a3e
Paul Hummer <paul@eventuallyanyway.com>
2013-07-15 02:06:44 -0600

    Update setup.py to work with the twisted plugin

--- setup.py
+++ setup.py
@@ -1,17 +1,66 @@
-import setuptools
+# Following code shamelessly stolen from http://is.gd/IKRoqV
+
+from distutils.core import setup
+
+# When pip installs anything from packages, py_modules, or ext_modules that
+# includes a twistd plugin (which are installed to twisted/plugins/),
+# setuptools/distribute writes a Package.egg-info/top_level.txt that includes
+# "twisted".  If you later uninstall Package with `pip uninstall Package`,
+# pip <1.2 removes all of twisted/ instead of just Package's twistd plugins.
+# See https://github.com/pypa/pip/issues/355 (now fixed)
+#
+# To work around this problem, we monkeypatch
+# setuptools.command.egg_info.write_toplevel_names to not write the line
+# "twisted".  This fixes the behavior of `pip uninstall Package`.  Note that
+# even with this workaround, `pip uninstall Package` still correctly uninstalls
+# Package's twistd plugins from twisted/plugins/, since pip also uses
+# Package.egg-info/installed-files.txt to determine what to uninstall,
+# and the paths to the plugin files are indeed listed in installed-files.txt.
+try:
+    from setuptools.command import egg_info
+    egg_info.write_toplevel_names
+except (ImportError, AttributeError):
+    pass
+else:
+    def _top_level_package(name):
+        return name.split('.', 1)[0]
+
+    def _hacked_write_toplevel_names(cmd, basename, filename):
+        pkgs = dict.fromkeys(
+            [_top_level_package(k)
+                for k in cmd.distribution.iter_distribution_names()
+                if _top_level_package(k) != "twisted"
+             ]
+        )
+        cmd.write_file("top-level names", filename, '\n'.join(pkgs) + '\n')
+
+    egg_info.write_toplevel_names = _hacked_write_toplevel_names
 
 
-setuptools.setup(
-    author=u'Paul Hummer',
-    author_email=u'paul@eventuallyanyway.com',
-    description=u'A server for handling Impact.js applications',
+#### Begin not-stolen code
+setup(
+    author='Paul Hummer',
+    author_email='paul@eventuallyanyway.com',
+    description='An ImpactJS development server.',
     install_requires=[
-        u'twisted',
+        'Twisted',
     ],
-    keywords=u'impactjs html5',
-    license=u'http://www.opensource.org/licenses/mit-license.php',
-    name=u'derrick',
-    packages=['derrick'],
-    url=u'https://github.com/rockstar/derrick',
-    version=u'0.1',
+    keywords='impactjs html5 games',
+    license='http://www.opensource.org/licenses/mit-license.php',
+    name='derrick',
+    packages=['derrick', 'twisted.plugins'],
+    url='https://github.com/rockstar/derrick',
+    version='0.1',
 )
+#### End not-stolen code
+
+
+# Make Twisted regenerate the dropin.cache, if possible.  This is necessary
+# because in a site-wide install, dropin.cache cannot be rewritten by
+# normal users.
+try:
+    from twisted.plugin import IPlugin, getPlugins
+except ImportError:
+    pass
+else:
+    list(getPlugins(IPlugin))
